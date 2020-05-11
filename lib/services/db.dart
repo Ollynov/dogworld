1f3cc98b324e42658f440ee0cc9254f9f5a083e1
ollynov <ollynov@gmail.com>
2020-05-10 17:55:27 -0700

    added globals file so we can always reference our modls transformer

--- lib/services/db.dart
+++ lib/services/db.dart
@@ -1,21 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:doggies/services/models.dart';
 import 'package:flutter/material.dart';
 
 class DatabaseService {
   final Firestore _db = Firestore.instance;
 
-  Future<Map> getQuiz(quizId) {
+  Future<Quiz> getQuiz(quizId) {
     return _db
         .collection('quizzes')
         .document(quizId)
         .get()
-        .then((snap) => snap.data);
+        .then((snap) => Quiz.fromMap(snap.data));
   }
 }
 
 loadData() async {
   var data = await DatabaseService().getQuiz('poi');
 
-  var formattedData = Text(data['description'] ?? 'backup string');
+  var formattedData = Text(data.description);
   return formattedData;
 }
