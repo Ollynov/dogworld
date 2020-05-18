594a7aaa5b4f3c56e62e70c9c7e36eb26b1cd6fe
ollynov <ollynov@gmail.com>
2020-05-17 23:38:19 -0700

    added all quiz page logic but need to adapt it with our dog quiz data model

--- lib/screens/quiz.dart
+++ lib/screens/quiz.dart
@@ -115,3 +115,157 @@ class StartPage extends StatelessWidget {
     );
   }
 }
+
+class CongratsPage extends StatelessWidget {
+  final Quiz quiz;
+  CongratsPage({this.quiz});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Congrats! You completed the ${quiz.title} quiz',
+            textAlign: TextAlign.center,
+          ),
+          Divider(),
+          Image.asset('assets/congrats.gif'),
+          Divider(),
+          FlatButton.icon(
+            color: Colors.green,
+            icon: Icon(FontAwesomeIcons.check),
+            label: Text(' Mark Complete!'),
+            onPressed: () {
+              _updateUserReport(quiz);
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                '/topics',
+                (route) => false,
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+Future<void> _updateUserReport(Quiz quiz) {
+  return Global.reportRef.upsert(
+    ({
+      'total': FieldValue.increment(1),
+      'topics': {
+        '${quiz.topic}': FieldValue.arrayUnion([quiz.id])
+      }
+    }),
+  );
+}
+
+class QuestionPage extends StatelessWidget {
+  final Question question;
+  QuestionPage({this.question});
+
+  @override
+  Widget build(BuildContext context) {
+    var state = Provider.of<QuizState>(context);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            alignment: Alignment.center,
+            child: Text(question.text),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: question.options.map((opt) {
+              return Container(
+                height: 90,
+                margin: EdgeInsets.only(bottom: 10),
+                color: Colors.black26,
+                child: InkWell(
+                  onTap: () {
+                    state.selected = opt;
+                    _bottomSheet(context, opt, state);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    child: Row(
+                      children: [
+                        Icon(
+                            state.getSelected == opt
+                                ? FontAwesomeIcons.checkCircle
+                                : FontAwesomeIcons.circle,
+                            size: 30),
+                        Expanded(
+                          child: Container(
+                            margin: EdgeInsets.only(left: 16),
+                            child: Text(
+                              opt.value,
+                              style: Theme.of(context).textTheme.body2,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+        )
+      ],
+    );
+  }
+
+  /// Bottom sheet shown when Question is answered
+  _bottomSheet(BuildContext context, Option opt, QuizState state) {
+    bool correct = opt.correct;
+
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 250,
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(correct ? 'Good Job!' : 'Wrong'),
+              Text(
+                opt.detail,
+                style: TextStyle(fontSize: 18, color: Colors.white54),
+              ),
+              FlatButton(
+                color: correct ? Colors.green : Colors.red,
+                child: Text(
+                  correct ? 'Onward!' : 'Try Again',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () {
+                  if (correct) {
+                    state.nextPage();
+                  }
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
