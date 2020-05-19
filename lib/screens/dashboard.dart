f76e8bcd42040c883f6fcb39c4d3634a54479547
ollynov <ollynov@gmail.com>
2020-05-19 02:25:46 -0700

    need to add top and bottom bar to login screen

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,4 +1,7 @@
 import 'dart:developer';
+import 'package:logger/logger.dart';
+import 'dart:convert';
+import 'dart:developer' as developer;
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -6,8 +9,33 @@ import '../services/services.dart';
 import '../shared/shared.dart';
 import 'package:provider/provider.dart';
 
-class DashboardScreen extends StatelessWidget {
-  final AuthService auth = AuthService();
+class DashboardScreen extends StatefulWidget {
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  AuthService auth = AuthService();
+  var logger = Logger();
+
+  @override
+  void initState() {
+    super.initState();
+    auth.getUser.then(
+      (user) {
+        if (user != null) {
+          logger.d("user");
+          developer.log(
+            'log me',
+            name: 'my.app.category',
+            error: jsonEncode(user),
+          );
+        } else {
+          Navigator.pushReplacementNamed(context, '/login');
+        }
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,19 +45,31 @@ class DashboardScreen extends StatelessWidget {
       return Scaffold(
         appBar: AppBar(
           title: Text('Dashboard'),
-          backgroundColor: Colors.blue,
+          backgroundColor: Theme.of(context).primaryColor,
         ),
         body: Center(
-            child: FlatButton(
-                child: Text('Logout'),
-                color: Colors.red,
-                onPressed: () async {
-                  log('ok you just signed out bruh');
-                  print('ok you just signed out bruh');
-                  await auth.signOut();
-                  Navigator.of(context)
-                      .pushNamedAndRemoveUntil('/', (route) => false);
-                })),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome There',
+              style: TextStyle(height: 1.5, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FlatButton(
+                  child: Text('Logout'),
+                  color: Colors.red[400],
+                  onPressed: () async {
+                    log('ok you just signed out bruh');
+                    print('ok you just signed out bruh');
+                    await auth.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/', (route) => false);
+                  }),
+            ),
+          ],
+        )),
         bottomNavigationBar: AppBottomNav(),
       );
     } else {
