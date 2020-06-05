74142ce251f6a452e143cb93e77e4d5eea7414bb
ollynov <ollynov@gmail.com>
2020-06-04 20:06:00 -0700

    added model for UserDetails

--- lib/services/globals.dart
+++ lib/services/globals.dart
@@ -9,10 +9,11 @@ class Global {
     Breed: (data) => Breed.convertFromFireBaseMap(data),
     Quiz: (data) => Quiz.convertFromFireBaseMap(data),
     Report: (data) => Report.convertFromFireBaseMap(data),
+    UserDetails: (data) => UserDetails.convertFromFireBaseMap(data),
   };
 
   // Firestore References for Writes
   static final Collection<Breed> breedsRef = Collection<Breed>(path: 'Breed');
-  static final UserData<Report> reportRef =
-      UserData<Report>(collection: 'reports');
+  static final UserData<Report> reportRef = UserData<Report>(collection: 'reports');
+  static final UserData<UserDetails> userDetailsRef = UserData<UserDetails>(collection: 'users');
 }
