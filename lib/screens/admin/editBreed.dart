870bfb55b019a0eaef8087cc03afe78f3e87d160
ollynov <ollynov@gmail.com>
2020-06-10 22:24:41 -0700

    ok not trying to share data anymore

--- lib/screens/admin/editBreed.dart
+++ lib/screens/admin/editBreed.dart
@@ -4,6 +4,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import './../../services/services.dart';
 import './../../shared/shared.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+
+class SelectedBreed {
+ String breedId = 'Affenpinscher';
+}
+
+var stream = Stream.fromIterable([SelectedBreed()]);
 
 
 class EditBreedScreen extends StatelessWidget {
@@ -18,22 +25,26 @@ class EditBreedScreen extends StatelessWidget {
 
 
     if (user != null) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Admin'),
+
+      return StreamProvider<SelectedBreed>.value(
+        value: stream,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Admin'),
+          ),
+          body: Center(
+            child: SizedBox(
+              width: 500,
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                Text('Select Breed to Edit'),
+                BreedListDropDown(),
+            ],
+          ),
+              )),
+          bottomNavigationBar: AppBottomNav(route: 2, inactive: false,),
         ),
-        body: Center(
-          child: SizedBox(
-            width: 500,
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-              Text('Select Breed to Edit'),
-              BreedListDropDown()
-          ],
-        ),
-            )),
-        bottomNavigationBar: AppBottomNav(route: 2, inactive: false,),
       );
     } else {
       return LoadingScreen();
@@ -54,34 +65,56 @@ class _BreedListDropDownState extends State<BreedListDropDown> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-       future: Collection<Breed>(path: 'allBreeds').getData(),
-       builder: (BuildContext context, AsyncSnapshot<List<Breed>> snapshot) { 
-         List<Breed> allBreeds = snapshot.data;
-      
-         return DropdownButton<String>(
-            value: dropdownValue,
-            icon: Icon(Icons.arrow_downward),
-            iconSize: 24,
-            elevation: 16,
-            style: TextStyle(color: Colors.black),
-            underline: Container(
-              height: 2,
-              color: Colors.black,
-            ),
-            onChanged: (String newValue) {
-              setState(() {
-                dropdownValue = newValue;
-              });
-            },
-            items: allBreeds.map<DropdownMenuItem<String>>((Breed value) {
-              return DropdownMenuItem<String>(
-                value: value.id,
-                child: Text(value.id),
+    return Column(
+      children: [
+        FutureBuilder(
+           future: Collection<Breed>(path: 'allBreeds').getData(),
+           builder: (BuildContext context, AsyncSnapshot<List<Breed>> snapshot) { 
+             List<Breed> allBreeds = snapshot.data;
+          
+             return DropdownButton<String>(
+                value: dropdownValue,
+                icon: Icon(Icons.arrow_downward),
+                iconSize: 24,
+                elevation: 16,
+                style: TextStyle(color: Colors.black),
+                underline: Container(
+                  height: 2,
+                  color: Colors.black,
+                ),
+                onChanged: (String newValue) {
+                  setState(() {
+                    dropdownValue = newValue;
+                  });
+
+                },
+                items: allBreeds.map<DropdownMenuItem<String>>((Breed value) {
+                  return DropdownMenuItem<String>(
+                    value: value.id,
+                    child: Text(value.id),
+                  );
+                }).toList(),
               );
-            }).toList(),
-          );
-       }
+           }
+        ),
+        BreedDetails(breedId: dropdownValue)
+      ],
     );
   }
+}
+
+class BreedDetails extends StatelessWidget {
+  final String breedId;
+  const BreedDetails({Key key, this.breedId}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    // var selectedBreed = Provider.of<SelectedBreed>(context);
+
+    return Container(
+      child: Text('Here is selected: $breedId'),
+    );
+  }
+
 }
