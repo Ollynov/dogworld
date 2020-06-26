875000856b31dba10c0bf161d87f37003baf1871
ollynov <ollynov@gmail.com>
2020-06-25 23:43:39 -0700

    fixed the url encode bug

--- lib/screens/breed.dart
+++ lib/screens/breed.dart
@@ -8,15 +8,14 @@ import 'package:provider/provider.dart';
 
 
 class BreedScreen extends StatelessWidget {
-  final String breedId;
+  String breedId;
   final Breed breed;
   BreedScreen({this.breedId, this.breed});
 
   
   @override
   Widget build(BuildContext context) {
-    print('here is what we got for breedID');
-    print(breedId);
+    breedId = Uri.decodeFull(breedId);
 
     // the args here is if we want to use data that we pass from the dogopedia screen. The big benefit here is one less API call, and simply pass in the data we already have. The downside is that if someone visits the page directly they will have no data. The best approach is a combo of both. 
     // final dynamic args = ModalRoute.of(context).settings.arguments;
