7a351752712f85d7d5efa48467be577fb298d97f
ollynov <ollynov@gmail.com>
2020-07-07 00:20:14 -0700

    having problem with running _getMoreBreeds

--- lib/screens/dogopedia.dart
+++ lib/screens/dogopedia.dart
@@ -1,18 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:doggies/services/models.dart';
 import 'package:doggies/services/services.dart';
 import 'package:doggies/shared/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:responsive_grid/responsive_grid.dart';
 
 
-class DogopediaScreen extends StatelessWidget {
+class DogopediaScreen extends StatefulWidget {
+  @override
+  _DogopediaScreenState createState() => _DogopediaScreenState();
+}
+
+class _DogopediaScreenState extends State<DogopediaScreen> {
+  List pagesOfData;
+  bool loading; 
+  int perPageLimit;
+  Breed lastDocument;
+
+  _getMoreBreeds() async {
+    print('running');
+    Query q = Firestore.instance.collection('Breed').startAfter([lastDocument]).limit(perPageLimit);
+    QuerySnapshot querySnapshot = await q.getDocuments();
+    print('ok query: ');
+    print(querySnapshot);
+    var newy = querySnapshot.documents[querySnapshot.documents.length - 1];
+
+    print('here is what our last doc looks like: ');
+    print(newy);
+    print('here are our new dogies: ');
+    print(querySnapshot.documents);
+    pagesOfData.add(querySnapshot.documents);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loading = true;
+    perPageLimit = 10;
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: Global.breedsRef.getData(10),
+      future: Global.breedsRef.getData(perPageLimit),
       builder: (BuildContext context, AsyncSnapshot snap) {
 
         if (snap.hasData) {
+          // setState(() {
+          //   loading = false;
+          //   lastDocument = snap.data[snap.data.length - 1];
+          // });
+           lastDocument = snap.data[snap.data.length - 1];
           List<Breed> breeds = snap.data;
           return Scaffold(
             appBar: AppBar(
@@ -51,6 +90,22 @@ class DogopediaScreen extends StatelessWidget {
                       )
                     ).toList()),
                   ),
+                  Row(
+                    children: [
+                      FlatButton(
+                        onPressed: () => _getMoreBreeds(),
+                        child: Icon(FontAwesomeIcons.arrowLeft),
+                        color: Colors.transparent,
+                        padding: EdgeInsets.only(top: 80, bottom: 80),
+                      ),
+                      FlatButton(
+                        onPressed: () => _getMoreBreeds(),
+                        child: Icon(FontAwesomeIcons.arrowRight),
+                        color: Colors.transparent,
+                        padding: EdgeInsets.only(top: 80, bottom: 80),
+                      )
+                    ],
+                  )
                 ],
               ),
             ),
