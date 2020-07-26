e60e43a682b8ec0806522eb352101e97f2122151
ollynov <ollynov@gmail.com>
2020-07-25 18:40:02 -0700

    refactored main page

--- lib/screens/admin/db.dart
+++ lib/screens/admin/db.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:doggies/services/models.dart';
 import 'package:doggies/services/services.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
